267d5df870411f032166b50e9164489063e42072
MoisesBrito31 <moisesbritofuentes@gmail.com>
2021-09-14 15:46:20 -0300

    criação do log de eventos

--- server/config/drive.py
+++ server/config/drive.py
@@ -5,6 +5,20 @@ from time import sleep
 from threading import Thread
 from .models import Camera, Imagem
 
+def gravaLog(tipo="Evento", msg="", file="log_imagem.txt"):
+    try:
+        arquivo = open(file,'r')
+        buffer = arquivo.read()
+        arquivo.close()
+    except:
+        buffer = ""
+    try:
+        now = str(datetime.now())
+        arquivoW = open(file,'w')
+        arquivoW.write(f'{buffer}[{now}] - [{tipo}] - {msg}\n\r')
+        arquivoW.close()
+    except:
+        pass
 
 class Protocol():
     HEADERSIZE = 100
@@ -54,7 +68,8 @@ class Protocol():
         resposta = 'falha'
         try:
             if not self.onLine:
-                print(f'tentando Conectar...\n')
+                print(f'tentando Conectar...')
+                gravaLog(msg=f'tentando Conectar...')
                 sleep(2)
                 try:
                     self.trans = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -64,35 +79,52 @@ class Protocol():
                     self.onLine = False
                     return resposta
             ret = self.trans.recv(64)
-            frame = int.from_bytes(ret[24:27],"little")
-            isJpeg = int.from_bytes(ret[32:33],"little")
-            img_size = int.from_bytes(ret[20:23],"little")
-            data = self.trans.recv(2048)
-            while img_size>len(data):
-                ret = self.trans.recv(2048)
-                data = data+ret
-
-            hoje = datetime.now()
-            idcam = self.IP.split('.')
-            nomeFile = f'{hoje.day}{hoje.month}{hoje.year}-{idcam[3]}-{frame}'
-            if isJpeg==1:
-                file = open(f'media/{nomeFile}.jpg','wb')
-                nomeFile = f'{nomeFile}.jpg'
+            if ret:
+                frame = int.from_bytes(ret[24:27],"little")
+                isJpeg = int.from_bytes(ret[32:33],"little")
+                img_size = int.from_bytes(ret[20:23],"little")
+                data = self.trans.recv(2048)
+                while img_size>len(data):
+                    ret = self.trans.recv(2048)
+                    if ret:
+                        data = data+ret
+                    else:
+                        gravaLog(tipo="Falha",msg="falha durente recebimento da imagem")
+                        return "falha durente recebimento da imagem"
+                hoje = datetime.now()
+                idcam = self.IP.split('.')
+                try:
+                    nomeFile = f'{hoje.day}{hoje.month}{hoje.year}-{idcam[3]}-{frame}'
+                    if isJpeg==1:
+                        file = open(f'media/{nomeFile}.jpg','wb')
+                        nomeFile = f'{nomeFile}.jpg'
+                    else:
+                        file = open(f'media/{nomeFile}.bmp','wb')
+                        nomeFile = f'{nomeFile}.bmp'
+                    file.write(data)
+                    file.close()
+                    gravaLog(msg=f'tamnho do arquivo: {len(data)} bytes')
+                    print(f'tamnho do arquivo: {len(data)} bytes')
+                    if len(data)>0:
+                        cam = Camera.objects.get(ip=self.IP)
+                        arq = Imagem(camera=cam,data=datetime.now(),img=f'media/{nomeFile}')
+                        cam.img = f'media/{nomeFile}'
+                        cam.save()
+                        arq.save()
+                    gravaLog(msg=f'Salvou image {nomeFile}')
+                    resposta = f'Salvou image {nomeFile}'
+                    self.onLine = True
+                    return resposta
+                except Exception as ex:
+                    self.onLine = False
+                    gravaLog(tipo="Falha",msg=f'falha na gravação - {str(ex)}')
+                    return f'falha na gravação - {str(ex)}'
             else:
-                file = open(f'media/{nomeFile}.bmp','wb')
-                nomeFile = f'{nomeFile}.bmp'
-            file.write(data)
-            file.close()
-            print(f'tamnho do arquivo: {len(data)} bytes')
-            if len(data)>0:
-                cam = Camera.objects.get(ip=self.IP)
-                arq = Imagem(camera=cam,data=datetime.now(),img=f'media/{nomeFile}')
-                cam.img = f'media/{nomeFile}'
-                cam.save()
-                arq.save()
-            resposta = f'Salvou image {nomeFile}'
-            return resposta
+                gravaLog(tipo="Falha",msg="retorno da imagem veio nula")
+                self.onLine = False
+                return "falha"
         except Exception as ex:
+            gravaLog(tipo="Falha Generica",msg=f'erro {str(ex)}')
             print(f'erro {str(ex)}')
             self.onLine = False
             cam = Camera.objects.get(ip=self.IP)
@@ -103,6 +135,7 @@ class Protocol():
 sleep(8)
 c1_img=Protocol('192.168.0.10',32200)
 
+gravaLog(msg='iniciou imagem...')
 print('iniciou imagem...')
 def c1_img_loop():
     while True:
--- server/config/ve_cont_garrafas.py
+++ server/config/ve_cont_garrafas.py
@@ -1,10 +1,25 @@
 import socket
-import os
-from datetime import datetime, timedelta
+from datetime import datetime
 from time import sleep
 from threading import Thread
 from pyModbusTCP.server import DataBank, ModbusServer
-from .models import Camera, Imagem
+from .models import Camera
+
+
+def gravaLog(tipo="Evento", msg="", file="log_contagem.txt"):
+    try:
+        arquivo = open(file,'r')
+        buffer = arquivo.read()
+        arquivo.close()
+    except:
+        buffer = ""
+    try:
+        now = str(datetime.now())
+        arquivoW = open(file,'w')
+        arquivoW.write(f'{buffer}[{now}] - [{tipo}] - {msg}\n\r')
+        arquivoW.close()
+    except:
+        pass
 
 class Protocol():
     HEADERSIZE = 100
@@ -22,8 +37,7 @@ class Protocol():
         self.reprovados = reprovados
         self.IP=ip
         self.PORT = port
-        self.trans = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#Criando um Soquete ,criar um INET, soquete STREAMing 
-       #O try/except serve para tratamento de exceções
+        self.trans = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         try:
             self.trans.connect((self.IP,self.PORT))
             self.onLine = True
@@ -31,10 +45,9 @@ class Protocol():
             self.onLine = False 
 
     def read_data(self):
-        resposta = 'falha'
         try:
             if not self.onLine:
-                print(f'tentando Conectar...\n')
+                gravaLog(msg="tentando se conectar...")
                 sleep(2)
                 try:
                     self.trans = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -42,16 +55,12 @@ class Protocol():
                     self.onLine = True
                 except:
                     self.onLine = False
-                    return resposta
-            resposta = self.trans.recv(self.HEADERSIZE).decode("utf-8")#Convert byte nos valores
-            print(f'a resposta é {resposta}')
+                    return "falha"
+            resposta = self.trans.recv(self.HEADERSIZE).decode("utf-8")
             if not resposta:
-                print('falha de processamento de recepção')
+                gravaLog(tipo="Falha de Recepção",msg=f'resposta = {resposta}')
                 self.onLine = False
-                #arquivo = open("data.log",'w')
-                #arquivo.write(f'{{"online":"{self.onLine}","total":{self.contagem},"aprovados":{self.aprovados},"reprovados":{self.reprovados}}}')
-                #arquivo.close
-                return resposta
+                return "falha"
             dados_arrey = str(resposta).split(',')
             self.faltantes+= int(dados_arrey[0])
             self.contagem+= 24-int(dados_arrey[0])
@@ -60,25 +69,19 @@ class Protocol():
             else:
                 self.aprovados+=1
             self.onLine = True
-            #arquivo = open("data.log",'w')
-            #arquivo.write(f'{{"online":"{self.onLine}","total":{self.contagem},"aprovados":{self.aprovados},"reprovados":{self.reprovados}}}')
-            #arquivo.close
             return dados_arrey
 
         except Exception as erro:
-            print(f'o erro foi {str(erro)}')
+            gravaLog(tipo="Falha geral da recepção",msg=f'o erro foi {str(erro)}')
             self.onLine = False 
-            #arquivo = open("data.log",'w')
-            #arquivo.write(f'{{"online":"{self.onLine}","total":{self.contagem},"aprovados":{self.aprovados},"reprovados":{self.reprovados}}}')
-            #arquivo.close
-            return resposta
+            return "falha"
     def zerar(self):
         self.faltantes=0
         self.contagem=0
         self.aprovados=0
         self.reprovados=0
 
-print("drive Contagem Iniciado")
+gravaLog(msg="drive Contagem Iniciado")
 memo = Camera.objects.get(ip="192.168.0.10")
 camera = Protocol("192.168.0.10",32100,faltantes=memo.faltantes,contagem=memo.garrafas,aprovados=memo.aprovado,reprovados=memo.reprovado)
 server = ModbusServer(host='0.0.0.0', port=502)
@@ -97,7 +100,7 @@ def gravaBanco():
         cam.garrafas = camera.contagem
         cam.save()
     except Exception as ex:
-        print(str(ex))
+       gravaLog(tipo="Falha ao gravar no banco",msg=str(ex),file="log_dados.txt")
 
 def ciclo():
     while True:
@@ -123,7 +126,8 @@ def ciclo_arquivo():
             Ls = int(camera.reprovados%65536)
             data.set_words(7,[Ms,Ls])
         except Exception as ex:
-            print(str(ex))
+            gravaLog(tipo="Falha ao gravar no ciclo",msg=str(ex),file="log_dados.txt")
+        """    
         arquivo = open("data.log",'w')
         arquivo.write(f'{{"online":"{camera.onLine}","faltantes":{camera.faltantes},"total":{camera.contagem},"aprovados":{camera.aprovados},"reprovados":{camera.reprovados}}}')
         arquivo.close
@@ -133,6 +137,8 @@ def ciclo_arquivo():
         if os.path.isfile('zerar.txt'):
             camera.zerar()
             os.remove('zerar.txt')
+        """
+
 
 
 
@@ -140,8 +146,10 @@ def ciclo_modbus():
     server.start()
 
 Tciclo = Thread(target=ciclo)
-Tciclo_arquivo = Thread(target=ciclo_arquivo)
 Tciclo.start()
+
+#Tciclo_modbus = Thread(target=ciclo_modbus)
+#Tciclo_modbus.start()
+
+Tciclo_arquivo = Thread(target=ciclo_arquivo)
 Tciclo_arquivo.start()
-Tciclo_modbus = Thread(target=ciclo_modbus)
-Tciclo_modbus.start()
