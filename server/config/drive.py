3105712198cb59c108cd691702bcf65d1250a62a
MoisesBrito31 <moisesbritofuentes@gmail.com>
2022-05-13 14:05:20 -0300

    acerto no drive dos servidores base

--- server/config/drive.py
+++ server/config/drive.py
@@ -1,10 +1,24 @@
-import socket
+import socket, threading
 import os
 from datetime import datetime, timedelta
 from time import sleep
 from threading import Thread
 from .models import Camera, Imagem
 from pyModbusTCP.client import ModbusClient as Modbus
+import subprocess
+
+fim_Programa = threading.Event()
+
+def atualizaRelogio():
+    while not fim_Programa.is_set():
+        sleep(10)
+        try:
+            arquivo = open("relogio.txt","w")
+            a = subprocess.check_output(['sudo','date','+%m%d%H%M%Y'])
+            arquivo.write(str(a[:12]))
+            arquivo.close()
+        except:
+            pass
 
 def gravaLog(tipo="Evento", msg="", file="log_imagem.txt"):
     try:
@@ -39,6 +53,16 @@ class Protocol():
         except:
             self.onLine = False 
 
+    def read_data_Modbus(self,endr,qtd):
+        resposta = 'falha'
+        try:
+            resposta = self.proto.read_input_registers(endr,qtd)
+            return resposta
+        except Exception as ex:
+            self.onLine = False
+            gravaLog(tipo="Falha",msg=f'falha modbus- {str(ex)}')
+            return f'falha modbus - {str(ex)}'
+
     def read_data(self):
         resposta = 'falha'
         try:
@@ -54,56 +78,61 @@ class Protocol():
                     return resposta
             resposta = self.trans.recv(self.HEADERSIZE).decode("utf-8")
             dados_arrey = str(resposta).split(',')
-            #cam = Camera.objects.get(ip=self.IP)
-            #cam.status= 'Online'
-            #cam.reprovado = dados_arrey[4]
-            #cam.aprovado = dados_arrey[3]
-            #cam.save()
-            return dados_arrey
+            cam = Camera.objects.get(ip=self.IP)
+            cam.status= 'Online'
+            cam.reprovado = dados_arrey[4]
+            cam.aprovado = dados_arrey[3]
+            cam.save()
+            return resposta
         except:
-            #cam = Camera.objects.get(ip=self.IP)
-            #cam.status= 'Offline'
-            #cam.save()
+            cam = Camera.objects.get(ip=self.IP)
+            cam.status= 'Offline'
+            cam.save()
             self.onLine = False 
             return resposta
-
-    def read_data_Modbus(self,endr,qtd):
-        resposta = 'falha'
-        try:
-            resposta = self.proto.read_input_registers(endr,qtd)
-            return resposta
-        except Exception as ex:
-            self.onLine = False
-            gravaLog(tipo="Falha",msg=f'falha modbus- {str(ex)}')
-            return f'falha modbus - {str(ex)}'
-
 
     def read_img(self):
         resposta = 'falha'
         try:
             if not self.onLine:
-                print(f'tentando Conectar...')
+                print(f'tentando Conectar camera {self.IP}...')
                 gravaLog(msg=f'tentando Conectar...')
                 sleep(2)
                 try:
                     self.trans = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
                     self.trans.connect((self.IP,self.PORT))
                     self.onLine = True
+                    gravaLog(msg=f'Conexão restabelecida!!')
                 except:
                     self.onLine = False
                     return resposta
             ret = self.trans.recv(64)
+            try:
+                valida = str(ret[0:15].decode('UTF-8'))
+                if valida.find("VE")<0:
+                    self.onLine = False
+                    self.trans.close()
+                    sleep(2)
+                    gravaLog(tipo="Falha",msg=f'Falha de Sync - não retornou VE')
+            except Exception as ex:
+                self.onLine = False
+                self.trans.close()
+                sleep(2)
+                gravaLog(tipo="Falha",msg=f'Falha de Sync - {str(ex)}')
+                return "Falha de Sync"
             if ret:
                 frame = int.from_bytes(ret[24:27],"little")
                 isJpeg = int.from_bytes(ret[32:33],"little")
                 img_size = int.from_bytes(ret[20:23],"little")
-                data = self.trans.recv(2048)
-                while img_size>len(data):
-                    ret = self.trans.recv(2048)
+                data = self.trans.recv(5000)
+                while img_size>len(data) and ret:
+                    ret = self.trans.recv(10000)
                     if ret:
                         data = data+ret
+                        print(f'{len(ret)}b dados recebidos, total de: {len(data)+64}b')
                     else:
                         gravaLog(tipo="Falha",msg="falha durente recebimento da imagem")
+                        self.onLine = False
                         return "falha durente recebimento da imagem"
                 hoje = datetime.now()
                 idcam = self.IP.split('.')
@@ -117,128 +146,106 @@ class Protocol():
                         nomeFile = f'{nomeFile}.bmp'
                     file.write(data)
                     file.close()
-                    gravaLog(msg=f'tamnho do arquivo: {len(data)} bytes')
-                    print(f'tamnho do arquivo: {len(data)} bytes')
-                    if len(data)>0:
+                    #gravaLog(msg=f'tamnho do arquivo: {len(data)} bytes')
+                    #print(f'tamnho do arquivo: {len(data)} bytes')
+                    if len(data)>65:
                         cam = Camera.objects.get(ip=self.IP)
                         arq = Imagem(camera=cam,data=datetime.now(),img=f'media/{nomeFile}')
                         cam.img = f'media/{nomeFile}'
                         cam.save()
                         arq.save()
-                    gravaLog(msg=f'Salvou image {nomeFile}')
-                    resposta = f'Salvou image {nomeFile}'
-                    self.onLine = True
-                    return resposta
+                        #gravaLog(msg=f'Salvou image {nomeFile}')
+                        resposta = f'Salvou image {nomeFile}'
+                        self.onLine = True
+                        return resposta
+                    else:
+                        gravaLog(tipo="Falha",msg=f'falha no processamento da img - len(data):{len(data)}')
+                        return "Falha"
                 except Exception as ex:
-                    self.onLine = False
+                    sleep(2)
                     gravaLog(tipo="Falha",msg=f'falha na gravação - {str(ex)}')
                     return f'falha na gravação - {str(ex)}'
             else:
                 gravaLog(tipo="Falha",msg="retorno da imagem veio nula")
                 self.onLine = False
+                sleep(2)
                 return "falha"
         except Exception as ex:
             gravaLog(tipo="Falha Generica",msg=f'erro {str(ex)}')
             print(f'erro {str(ex)}')
             self.onLine = False
-            #cam = Camera.objects.get(ip=self.IP)
-            #cam.save()
+            cam = Camera.objects.get(ip=self.IP)
+            cam.save()
+            sleep(2)
             return resposta
 
+def c1_img_loop(cam):
+    print(f'iniciou camera {cam.IP}')
+    while not fim_Programa.is_set():
+        print(cam.read_img())
 
-sleep(8)
-
-controle = True
-
-c1=Protocol('192.168.0.12',32200)
-c2=Protocol('192.168.0.13',32200)
-
-gravaLog(msg='iniciou imagem...')
-print('iniciou imagem...')
-
-def c1_img_loop():
-    while controle:
-        print(c1.read_img())
-
-#c1_img_thr = Thread(target=c1_img_loop)
-#c1_img_thr.start()
-
-def c1_data_loop():
-    while controle:
-        valor =c1.read_data_Modbus(8,4)
+def c_data_loop(cam):
+    print(f'iniciou camera {cam.IP}')
+    while not fim_Programa.is_set():
+        valor =cam.read_data_Modbus(8,4)
         if valor:
             #print(f'retorno:{valor}')
             aprovados = valor[0]+valor[1]*65536
             reprovados = valor[2]+valor[3]*65536
             #print(f'aprovados: {aprovados} , reprovados: {reprovados}')
             try:
-                cam = Camera.objects.get(ip=c1.IP)
-                cam.status= 'Online'
-                cam.reprovado = reprovados
-                cam.aprovado = aprovados
-                cam.save()
+                came = Camera.objects.get(ip=cam.IP)
+                came.status= 'Online'
+                came.reprovado = reprovados
+                came.aprovado = aprovados
+                came.save()
             except:
                 pass
         else:
             #print('camera OffLine')
             try:
-                c1.proto = Modbus(host=c1.IP,port=502)
-                c1.proto.open()
-                cam = Camera.objects.get(ip=c1.IP)
+                cam.proto = Modbus(host=cam.IP,port=502)
+                cam.proto.open()
+                cam = Camera.objects.get(ip=cam.IP)
                 cam.status= 'OffLine'
                 cam.save()
             except:
                 pass
         sleep(1)
-c1_data_thr = Thread(target=c1_data_loop)
-c1_data_thr.start()   
 
 
-def c2_data_loop():
-    while controle:
-        valor =c2.read_data_Modbus(8,4)
-        if valor:
-            #print(f'retorno:{valor}')
-            aprovados = valor[0]+valor[1]*65536
-            reprovados = valor[2]+valor[3]*65536
-            #print(f'aprovados: {aprovados} , reprovados: {reprovados}')
-            try:
-                cam = Camera.objects.get(ip=c2.IP)
-                cam.status= 'Online'
-                cam.reprovado = reprovados
-                cam.aprovado = aprovados
-                cam.save()
-            except:
-                pass
+def mainCicle():
+    gravaLog(msg='iniciou imagem...')
+    print('iniciou drive...')
+    cameras = []
+    thrs = []
+    camQuery = Camera.objects.all()
+    for c in camQuery:
+        camera = Protocol(c.ip,c.porta_img)
+        thrs.append(Thread(target=c1_img_loop,args=(camera,)))
+        thrs.append(Thread(target=c_data_loop,args=(camera,)))
+    for t in thrs:
+        t.start()
+    t_relogio = threading.Thread(target=atualizaRelogio).start()
+    while not fim_Programa.is_set():
+        cmd = input()
+        if cmd == "end":
+            fim_Programa.set()
         else:
-            #print('camera OffLine')
-            try:
-                c2.proto = Modbus(host=c2.IP,port=502)
-                c2.proto.open()
-                cam = Camera.objects.get(ip=c2.IP)
-                cam.status= 'OffLine'
-                cam.save()
-            except:
-                pass
-        sleep(1)
-c2_data_thr = Thread(target=c2_data_loop)
-c2_data_thr.start()   
+            print("comando inexistente")
+    print("fim do programa")
 
+try:
+    fi = open("relogio.txt","r")
+    valor = str(fi.read())[2:14]
+    print(valor)
+    comando = subprocess.check_output(['sudo','date',valor])
+    fi.close()
+    print(f'retorno do relogio: {comando}')
+except Exception as ex:
+    print(f'falha em set do relogio: {str(ex)}')
 
-def c2_img_loop():
-    while controle:
-        print(c2.read_img())
-
-#c2_img_thr = Thread(target=c2_img_loop)
-#c2_img_thr.start()
-
-
-
-
-
-
-
-
+t_main = threading.Thread(target=mainCicle).start()
 
 
 
